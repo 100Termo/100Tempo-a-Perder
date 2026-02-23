256e7a7b65f03bca60efb099119c34b7103fb30d
100Termo <262913983+100Termo@users.noreply.github.com>
2026-02-23 00:40:07 +0000

    Apenas teste

--- TEste.c
+++ TEste.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+
+    printf("Digite sua matricula: ");
+    scanf("%d", &matricula);
+    printf("\n");
+
+    printf("Nome do aluno: %s \nMatricula: %d \n", nome, matricula);
+    printf("Idade: %d \nAltura: %.2f \n", idade, altura);
+
+    return 0;
+}
